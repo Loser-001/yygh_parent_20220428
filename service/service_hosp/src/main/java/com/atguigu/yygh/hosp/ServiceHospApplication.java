20ebd7dfeaf56e9596134f17f680f6b60b5b0fff
Loser-001 <1669279462@qq.com>
2022-03-02 15:44:59 +0800

    第一次提交20220216

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -1,8 +1,22 @@
 package com.atguigu.yygh.hosp;
 
+//导入这两个包后，就会变成绿叶
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author Jerry
  * @create 2022-02-16 21:45
  */
+@SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu")
+@EnableDiscoveryClient  //使用nacos进行注册
+@EnableFeignClients(basePackages = "com.atguigu") //远程调用service_cmn_client
 public class ServiceHospApplication {
+    public static void main(String[] args) {
+    SpringApplication.run(ServiceHospApplication.class,args);
+    }
 }
